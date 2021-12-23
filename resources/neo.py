9efd47a2000ff41cb7ccb0fb52a69651140114aa
rhys <rhys.newbury@monash.edu>
2021-12-23 14:08:48 +1100

    hand tuned controller

--- resources/neo.py
+++ resources/neo.py
@@ -11,6 +11,8 @@ import roboticstoolbox as rtb
 import spatialmath as sm
 import numpy as np
 import qpsolvers as qp
+from itertools import product
+
 
 
 def transform_between_vectors(a, b):
@@ -25,7 +27,7 @@ def transform_between_vectors(a, b):
 
 # Launch the simulator Swift
 env = swift.Swift()
-env.launch(realtime=False, headless=True)
+env.launch(realtime=False, headless=False)
 
 # Create a Panda robot object
 panda = rtb.models.Panda()
@@ -33,8 +35,8 @@ panda = rtb.models.Panda()
 # Set joint angles to ready configuration
 panda.q = panda.qr
 
-_total = 0
-_totalSeen = 0
+_total = []
+_totalSeen = []
 
 # Number of joint in the panda which we are controlling
 n = 7
@@ -49,11 +51,16 @@ success = 0
 
 considerCollisions = True
 
-for i in range(100):
-    panda.q = panda.qr
+NUM_OBJECTS = 8
 
+collisions = []
+spheres = []
+
+
+
+for i in range(NUM_OBJECTS):
     angle = np.random.uniform() * np.pi * 2
-    radius = 0.5  # np.random.uniform() / 2 + 0.5
+    radius = np.random.uniform() / 4 + 0.25
     target_pos = np.array([radius * np.cos(angle), radius * np.sin(angle), 0])
 
     middle = (camera_pos + target_pos) / 2
@@ -65,21 +72,43 @@ for i in range(100):
     # line of sight between camera and object we want to avoid
     s0 = sg.Cylinder(
         radius=0.001,
-        length=np.linalg.norm(camera_pos - target_pos),
+        length=2, #np.linalg.norm(camera_pos - target_pos),
         base=sm.SE3(middle) * R,
     )
-
-    collisions = [s0]
+    collisions.append(s0)
 
     # Make a target
     target = sg.Sphere(
-        radius=0.02, base=sm.SE3(target_pos[0], target_pos[1], target_pos[2])
+        radius=0.02, base=sm.SE3(*target_pos)
     )
+    spheres.append(target)
 
-    # Add the Panda and shapes to the simulator
-    env.add(panda)
     env.add(s0)
     env.add(target)
+
+env.add(panda)
+
+for i in range(30):
+    panda.q = panda.qr
+
+    for k in range(NUM_OBJECTS):
+        angle = np.random.uniform() * np.pi * 2
+        radius = np.random.uniform() / 4 + 0.25
+        target_pos = np.array([radius * np.cos(angle), radius * np.sin(angle), 0])
+
+        middle = (camera_pos + target_pos) / 2
+        R, _, _ = transform_between_vectors(
+            np.array([0.0, 0.0, 1.0]), camera_pos - target_pos
+        )
+
+        collisions[k]._base = (sm.SE3(middle) * R).A
+        spheres[k]._base = sm.SE3(*target_pos).A
+
+        # collisions[i].length = np.linalg.norm(camera_pos - target_pos)
+
+    # Add the Panda and shapes to the simulator
+    # env.add(panda)    
+    # env.add(target)
 
     # Set the desired end-effector pose to the location of target
     Tep = panda.fkine(panda.q)
@@ -109,7 +138,8 @@ for i in range(100):
         Y = 0.01
 
         # Quadratic component of objective function
-        Q = np.eye(n + 7)
+        # joint angles + (position + rotation) + number of objects
+        Q = np.eye(n + 6 + NUM_OBJECTS)
 
         # Joint velocity component of Q
         Q[:n, :n] *= Y
@@ -118,18 +148,21 @@ for i in range(100):
         Q[n : n + 3, n : n + 3] = 10000 * (1 / np.power(et, 3)) * np.eye(3)
 
         # dealing with rotation
-        Q[n + 3 : -1, n + 3 : -1] = (1 / (np.power(et, 6))) * np.eye(3)
+        Q[n + 3 : n + 6, n + 3 : n + 6] = (1 / (np.power(et, 6))) * np.eye(3)
 
         # make the collisions a soft constraint
-        Q[-1, -1] = 100000 * np.power(et, 3)
+        # by introducing a slack term for each of the objects
+        for j in range(NUM_OBJECTS):
+            Q[-j, -j] = 100000 * np.power(et, 3)
+
 
         # The equality contraints
-        Aeq = np.c_[panda.jacobe(panda.q), np.eye(6), np.zeros((6, 1))]
+        Aeq = np.c_[panda.jacobe(panda.q), np.eye(6), np.zeros((6, NUM_OBJECTS))]
         beq = v.reshape((6,))
 
         # The inequality constraints for joint limit avoidance
-        Ain = np.zeros((n + 7, n + 7))
-        bin = np.zeros(n + 7)
+        Ain = np.zeros((n + 6 + NUM_OBJECTS, n + 6 + NUM_OBJECTS))
+        bin = np.zeros(n + 6 + NUM_OBJECTS)
 
         # The minimum angle (in radians) in which the joint is allowed to approach
         # to its limit
@@ -142,9 +175,9 @@ for i in range(100):
         # Form the joint limit velocity damper
         Ain[:n, :n], bin[:n] = panda.joint_velocity_damper(ps, pi, n)
 
-        occluded = False
+        occluded = [False] * NUM_OBJECTS
         # For each collision in the scene
-        for collision in collisions:
+        for (index, collision) in enumerate(collisions):
 
             # Form the velocity damper inequality contraint for each collision
             # object on the robot to the collision in the scene
@@ -161,8 +194,12 @@ for i in range(100):
             # If there are any parts of the robot within the influence distance
             # to the collision in the scene
             if c_Ain is not None and c_bin is not None and considerCollisions:
+                slack_matrix = np.zeros((c_Ain.shape[0], NUM_OBJECTS))
+                slack_matrix[:, index] = -np.ones((c_Ain.shape[0]))
+                # print(slack_matrix)
+                # input()
                 c_Ain = np.c_[
-                    c_Ain, np.zeros((c_Ain.shape[0], 6)), -np.ones((c_Ain.shape[0], 1))
+                    c_Ain, np.zeros((c_Ain.shape[0], 6)), slack_matrix
                 ]
 
                 # Stack the inequality constraints
@@ -171,18 +208,29 @@ for i in range(100):
 
             # print(d_in)
             if isinstance(d_in, float):
-                occluded = occluded or d_in < 0
+                occluded[index] = d_in < 0
             elif d_in is None:
-                occluded = True
+                occluded[index] = False
             else:
-                occluded = occluded or min(d_in) < 0
+                occluded[index] = min(d_in) < 0
 
         # Linear component of objective function: the manipulability Jacobian
-        c = np.r_[-panda.jacobm(panda.q).reshape((n,)), np.zeros(7)]
+        c = np.r_[-panda.jacobm(panda.q).reshape((n,)), np.zeros(6 + NUM_OBJECTS)]
 
         # The lower and upper bounds on the joint velocity and slack variable
-        lb = -np.r_[panda.qdlim[:n], 10 * np.ones(7)]
-        ub = np.r_[panda.qdlim[:n], 10 * np.ones(7)]
+        lb = -np.r_[panda.qdlim[:n], 10 * np.ones(6), np.zeros(NUM_OBJECTS)]
+        ub = np.r_[panda.qdlim[:n], 10 * np.ones(6 + NUM_OBJECTS)]
+
+        # print(Q.shape)
+        # print(c.shape)
+        # print(Ain.shape)
+        # print(Ain.shape)
+        # print(bin.shape)
+        # print(Aeq.shape)
+        # print(beq.shape)
+
+        # print(lb.shape)
+        # print(ub.shape)
 
         # Solve for the joint velocities dq
         qd = qp.solve_qp(Q, c, Ain, bin, Aeq, beq, lb=lb, ub=ub)
@@ -201,14 +249,19 @@ for i in range(100):
 
     time = 0
     arrived = False
+    number_objects_seen = 0
     while not arrived:
         try:
             arrived, occluded = step()
-            total_seen += not occluded
-            total += 1
+            total_seen += NUM_OBJECTS - sum(occluded)
+            # print(occluded)
+            # input()
+
+            # total_seen += not occluded
+            total += NUM_OBJECTS
             time += dt
 
-            if time > 4:
+            if time > 6:
                 print("sim timed out")
                 break
         except Exception as e:
@@ -217,16 +270,22 @@ for i in range(100):
 
             # print("qp could not solve", e)
             break
-    env.restart()
+    # input("arrived")
+    # try:
+    # env.restart()
+    # except:
+    #     pass
 
     success += arrived
     try:
         print(
-            f"success: {success/(i+1)}", f"{total_seen / total * 100}%", f"{time:.2f}s"
+            f"success: {success/(i+1)}", f"{NUM_OBJECTS * total_seen / total} number objects seen on average", f"{time:.2f}s"
         )
-        _total += total
-        _totalSeen += total_seen
     except:
         pass
+    
+    _total += [total]
+    _totalSeen += [total_seen]
 
-print(f"average: {100 * _totalSeen / _total}%")
+# print(f"average: {100 * _totalSeen / _total}%")
+print(np.mean(np.divide(_totalSeen, _total)) * NUM_OBJECTS)
